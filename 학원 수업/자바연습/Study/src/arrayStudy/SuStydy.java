6892dcd27c14a83608cd5536e9eb5c30899087dc
dododonng <k.do.dodong@gmail.com>
2023-05-31 01:11:26 +0900

    .

---
+++ 학원 수업/자바연습/Study/src/arrayStudy/SuStydy.java
@@ -0,0 +1,21 @@
+package arrayStudy;
+
+public class SuStydy {
+
+	public static void main(String[] args) {
+		
+			int i = 0;
+			
+			boolean ii = true;
+			
+			while(ii) {
+				i++;
+				if(i==123) {
+					System.out.println("while문을 종료합니다.");
+				}
+				break;
+			}
+		}
+	}
+
+
--- 학원 수업/자바연습/Study/src/asas.java
+++ 학원 수업/자바연습/Study/src/asas.java
@@ -3,6 +3,10 @@ public class asas {
 	
 	public static void main(String[] args) {
 		
+		Study ss = new Study();
+		ss.method();
+		
+		
         int[] sizeArray = new int[10];
         for ( int i = 0; i < sizeArray.length; i++) {
             sizeArray[i] = 250 + (5 * i);
