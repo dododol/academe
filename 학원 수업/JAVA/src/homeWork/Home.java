7d1ba0bf5f6c3aac053823bdfc00935c221cbe21
DESKTOP-DOL60IT\PC-15 <k.do.dodong@gmail.com>
2023-07-07 19:33:34 +0900

    .

--- 학원 수업/JAVA/src/homeWork/Home.java
+++ 학원 수업/JAVA/src/homeWork/Home.java
@@ -8,175 +8,464 @@ public class Home {
 		 */
 		// 문제 1, 정수형 타입 변수 a1을 선언해주세요.
 		
+		int a1;
+		
 		// 문제 2, 정수형 타입 변수 a2을 선언해주세요.
+		
+		int a2;
 		
 		// 문제 3, 변수 a1에 정수 값 50을 초기화해주세요.
 		
+		a1 = 50;
+		
 		// 문제 4, 변수 a2에 정수 값 30을 초기화해주세요.
+		
+		a2 = 30;
 		
 		// 문제 5, 문자열 타입 변수 str1을 선언해주세요.
 		
+		String str1;
+		
 		// 문제 6, 문자열 타입 변수 str2을 선언해주세요.
+		
+		String str2;
 		
 		// 문제 7, 문자열 타입 변수 str3을 선언해주세요.
 		
+		String str3;
+		
 		// 문제 8, 논리형 타입 변수 flag1을 선언해주세요.
+		
+		boolean flag1;
 		
 		// 문제 9, 논리형 타입 변수 flag2를 선언해주세요.
 		
+		boolean flag2;
+		
 		// 문제 10, 변수 str1에 값 "Hello"로 초기화해주세요.
+		
+		str1 = "Hello";
 		
 		// 문제 11, 변수 str2에 값 "World!"로 초기화해주세요.
 		
+		str2 = "World!";
+		
 		// 문제 12, 변수 flag1에 false로 초기화해주세요.
+		
+		flag1 = false;
 		
 		// 문제 13, 변수 flag2에 true로 초기화해주세요.
 		
+		flag2 = true;
+		
 		// 문제 14, 실수형 타입 중, f로 시작하는 타입의 변수 f1을 3.1로 초기화해주세요.
 		
+		float f1 = 3.1f;
+		
 		// 문제 15, 실수형 타입 중, f로 시작하는 타입의 변수 f2를 3.14로 초기화해주세요.
+
+		float f2 = 3.14f;
 		
 		// 문제 16, 실수형 타입 중, d로 시작하는 타입의 변수 d1을 0.04로 초기화해주세요.
 		
+		double d1 = 0.04;
+		
 		// 문제 17, 실수형 타입 중, d로 시작하는 타입의 변수 d2을 3.141592로 초기화해주세요.
+		
+		double d2 = 3.141592;
 		
 		// 문제 18, 변수 a1에 숫자 20을 더한 값을 변수 a1에 초기화해주세요.
 		
+		a1 += 20;
+		
 		// 문제 19, 변수 a2에 숫자 40을 더한 값을 변수 a2에 초기화해주세요.
+		
+		a2 += 40;
 		
 		// 문제 20, 변수 a2에 a1을 더한 값을 변수 a2에 초기화해주세요.
 		
+		a2 += a1;
+		
 		// 문제 21, 변수 str1에 문자열 "_str1"를 추가 후 변수 str1에 초기화해주세요.
+		
+		str1 = "_str1";
 		
 		// 문제 22, 변수 str2에 문자열 "_str2"를 추가 후 변수 str2에 초기화해주세요.
 		
+		str2 = "_str2";
+		
 		// 문제 23, 변수 str3에 변수 str1 + str2를 추가 후 변수 str3에 초기화해주세요.
 		
+		str3 = str1+str2;
+				
 		// 문제 24, 변수 flag1에 true로 초기화해주세요.
+		
+		flag1 = true;
 		
 		// 문제 25, 변수 flag2에 false로 초기화해주세요.
 		
+		flag2 = false;
+		
 		// 문제 26, 변수 f1에 0.01을 더한 값을 변수 f1에 초기화해주세요.
+		
+		f1 += 0.01;
 		
 		// 문제 27, 변수 f2에 3.14를 더한 값을 변수 f2에 초기화해주세요.
 		
+		f2 += 3.14;
+		
 		// 문제 28, 변수 f1에 자기자신을 더한 값을 변수 f1에 초기화해주세요.
+		
+		f1 += f1;
 		
 		// 문제 29. 변수 f2에 자기자신을 더한 값을 변수 f2에 초기화해주세요.
 		
+		f2 += f2;
+		
 		// 문제 30. 변수 d1에 0.001을 더한 값을 d1에 초기화해주세요.
+		
+		d1 += 0.001;
 		
 		// 문제 31. 변수 d2에 0.005를 더한 값을 d2에 초기화해주세요.
 		
+		d2 += 0.005;
+		
 		// 문제 32. 변수 d1에 0.5를 더한 값을 d1에 초기화해주세요.
+		
+		d1 += 0.5;
 		
 		// 문제 33. 변수 d2에 d1을 더한 값을 d2에 초기화해주세요.
 		
+		d2 = d1+d2;
+		
 		// 문제 34. 변수 a1,a2,str1,str2,str3,flag1,flag2,f1,f2,d1,d2 변수 모두를 출력해주세요.
+		
+		System.out.println("a1 ==> " + a1);
+		System.out.println("a2 ==> " + a2);
+		System.out.println("str1  ==> " + str1);
+		System.out.println("str2  ==> " + str2);
+		System.out.println("str3  ==> " + str3);
+		System.out.println("flag1 ==> " + flag1);
+		System.out.println("flag2 ==> " + flag2);
+		System.out.println("f1 ==> " + f1);
+		System.out.println("f2 ==> " + f2);
+		System.out.println("d1 ==> " + d1);
+		System.out.println("d2 ==> " + d2);
 		
 		// 문제 35. 정수형 변수 score를 값 77로 선언 및 초기화해주세요.
 		
+		int score = 77;
+		
 		// 문제 36. 문자열 변수 grade를 값 ""로 선언 및 초기화해주세요.
+		
+		String grade = "";
 		
 		// 문제 37. 문자열 변수 name을 값으로 본인의 이름으로 선언 및 초기화해주세요.
 		
+		String name = "김태형";
+		
 		// 문제 38. 문자열 변수 gender를 값으로 본인의 성별로 선언 및 초기화해주세요.
+		
+		String gender = "남자";
 		
 		// 문제 39. 정수형 변수 age를 값으로 본인의 나이로 선언 및 초기화해주세요.
 		
+		int age = 29;
+		
 		// 문제 40. 문자열 변수 introduction를 값으로 자신의 포부로 선언 및 초기화해주세요.
+		
+		String introduction = "아자";
 		
 		
 		/*
 		 * 조건문 연습 (40선)
 		 */
 		// 문제 1. 변수 a1이 50보다 작은 숫자인 경우 "a1은 작습니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(a1 < 50) {
+			System.out.println("a1은 작습니다");
+		}else {
+			System.out.println("a1이 큽니다");
+		}
 		
 		// 문제 2. 변수 a2가 50보다 큰 숫자인 경우 "a2는 큽니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(a2 < 50) {
+			System.out.println("a2은 작습니다");
+		}else {
+			System.out.println("a2이 큽니다");
+		}
 		
 		// 문제 3. 변수 a1이 10보다 크고 50보다 작은 숫자인 경우 "a1은 10보다 크고 50보다 작습니다"를 출력해주세요.
+
+		if(a1 > 10 && a1 <50) {
+			System.out.println("a1은 10보다 크고 50보다 작습니다");
+		}
 		
 		// 문제 4. 변수 a2가 50보다 크고 100보다 작은 숫자인 경우 "a2는 50보다 크고 100보다 작습니다"를 출력해주세요.
+
+		if(a2 >50 && a2 < 100) {
+			System.out.println("s2는 50보다 크고 100보다 작습니다");
+		}
 		
 		// 문제 5. 변수 a1과 a2가 같은 숫자인 경우 "a1과 a2는 같습니다"를 출력해주세요.
+
+		if(a1 == a2) {
+			System.out.println("a1과 a2는 같습니다");
+		}
 		
 		// 문제 6. 변수 a1이 30보다 작거나 a2가 30보다 작을 때 "a1또는 a2가 30보다 작습니다"를 출력해주세요.
+
+		if(a1 < 30 || a2 < 30) {
+			System.out.println("a1또는 a2가 30보다 작습니다");
+		}
 		
 		// 문제 7. 변수 a1이 a2보다 작을때 "a1은 a2보다 작습니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(a1 < a2) {
+			System.out.println("a1은 a2보다 작습니다");
+		}
 		
 		// 문제 8. 변수 a1이 a2+10보다 클때 "a1은 a2+10보다 큽니다"를 출력해주세요.
+
+		if(a1 > (a2+10)) {
+			System.out.println("a1은 a2+10보다 큽니다");
+		}
 		
 		// 문제 9. 변수 a1이 100이상일때는 "a1은 100 이상입니다"를 출력하고, 변수 a1이 100이상이 아닐때 "a1은 100이상이 아닙니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(a1 >= 100) {
+			System.out.println("a1은 100 이상입니다");
+		}else {
+			System.out.println("a1은 100 이상이 아닙니다");
+		}
 		
 		// 문제 10. 변수 a2가 0이면 "a2는 0입니다"를 출력하고, a2가 0이 아니면 "a2는 0이 아닙니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(a2 == 0) {
+			System.out.println("a2는 0입니다");
+		}else {
+			System.out.println("a2는 0이 아닙니다");
+		}
 		
 		// 문제 11. 변수 a1과 a2 모두 0이면 "a1,a2는 0입니다"를 출력하고, a1과 a2 모두 0이 아닌경우 "a1,a2는 0이 아닙니다"를 출력해주새요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(a1 == 0 && a2 == 0) {
+			System.out.println("a1,a2는 0입니다");
+		}else {
+			System.out.println("a1,a2는 0이 아닙니다");
+		}
 		
 		// 문제 12. 변수 str1의 값이 ""인 경우 "str1은 공백입니다"를 출력해주세요.
+
+		if(str1 == "") {
+			System.out.println("str1은 공백입니다");
+		}
 		
 		// 문제 13. 변수 str2의 값이 ""인 경우 "str2는 공백입니다"를 출력해주세요.
+
+		if(str2 == "") {
+			System.out.println("str2는 공백입니다");
+		}
 		
 		// 문제 14. 변수 str3의 값이 ""인 경우 "str3는 공백입니다"를 출력해주세요.
+
+		if(str3 == "") {
+			System.out.println("str3은 공백입니다");
+		}
 		
 		// 문제 15. 변수 str1의 값이 "Hello"인 경우 "str1은 Hello입니다"를 출력해주세요.
+
+		if(str1 == "Hello") {
+			System.out.println("str1은 Hello입니다");
+		}
 		
 		// 문제 15. 변수 str2의 값이 "World"인 경우 "str2는 World입니다"를 출력해주세요.
+
+		if(str2 == "World") {
+			System.out.println("str2는 World입니다");
+		}
 		
 		// 문제 16. 변수 str3의 값이 "Hello World"인 경우 "str3는 Hello World입니다"를 출력해주세요.
+
+		if(str3 == "Hello World") {
+			System.out.println("str3은 Hello World입니다");
+		}
 		
 		// 문제 17. 변수 str1의 값이 "Hello"인 경우 "str1의 값 일치"를, "Hello"가 아닌 경우 "str1의 값 불일치"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(str1 == "Hello") {
+			System.out.println("str1의 값 일치");
+		} else {
+			System.out.println("str1의 값 불일치");
+		}
 		
 		// 문제 18. 변수 str2의 값이 "World"인 경우 "str2의 값 일치"를, "World"가 아닌 경우 "str2의 값 불일치"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(str2 == "World") {
+			System.out.println("str2의 값 일치");
+		} else {
+			System.out.println("str2의 값 불일치");
+		}
 		
 		// 문제 19. 변수 str3의 값이 ""인 경우 "str3는 공백입니다"를 출력하고, ""이 아닌 경우 "str3는 공백이 아닙니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(str3 == "") {
+			System.out.println("str3은 공백입니다");
+		} else {
+			System.out.println("str3은 공백이 아닙니다");
+		}
 		
 		// 문제 20. flag1의 값이 false면 "flag1은 false입니다"를 출력해주세요.
+
+		if(flag1 == false) {
+			System.out.println("flag1은 false입니다");
+		}
 		
 		// 문제 21. flag2의 값이  true면 "flag2는 true입니다"를 출력해주세요.
+
+		if(flag2 == true) {
+			System.out.println("flag2는 true입니다");
+		}
 		
 		// 문제 22. flag1의 값이 true면 flag1의 값을 false로 변경하고 false면 flag1의 값을 true로 변경해주세요.
+
+		if(flag1 == true) {
+			flag1 = false;
+		} else {
+			flag1 = true;
+		}
 		
 		// 문제 23. flag2의 값이 false면 flag2의 값을 true로 변경하고 flage2의 값이 true면 flag2의 값을 false로 변경해주세요.
+
+		if(flag2 == false) {
+			flag2 = true;
+		} else {
+			flag2 = false;
+		}
 		
 		// 문제 24. f1의 값이 5보다 크면 "f1은 5보다 큽니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(f1 > 5) {
+			System.out.println("f1은 5보다 큽니다");
+		}
 		
 		// 문제 25. f1의 값이 5보다 작으면 "f1은 5보다 작습니다"를 출력해주세요.
+
+		if(f1 < 5) {
+			System.out.println("f1은 5보다 작습니다");
+		}
 		
 		// 문제 26. f2의 값이 10보다 크면 "f2는 10보다 큽니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(f2 > 10) {
+			System.out.println("f2는 10보다 큽니다");
+		}
 		
 		// 문제 27. f2의 값이 10보다 작으면 "f2는 10보다 작습니다"를 출력해주세요.
+
+		if(f2 < 10) {
+			System.out.println("f2는 10보다 작습니다");
+		}
 		
 		// 문제 28. f1의 값이 5이상 10 미만인 경우 "f1은 5이상 10미만입니다"를 출력하고, 그 두가지 조건도 아닌 경우 
 		// 			"해당사항 없습니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(f1 >= 5 && f1 <10) {
+			System.out.println("f1은 5이상 10미만입니다");
+		} else {
+			System.out.println("해당사항 없습니다");
+		}
 		
 		// 문제 29. f2의 값이 10이상 20 이하인 경우 "f2은 10이상 20이하입니다"를 출력하고, 그 두가지 조건도 아닌 경우 
 		// 			"해당사항 없습니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(f2 >= 10 && f2 <= 20) {
+			System.out.println("f2는 10이상 20이하입니다");
+		} else {
+			System.out.println("해당사항 없습니다");
+		}
 		
 		// 문제 30. d1의 값이 3.01보다 큰 경우 "d1은 3.01보다 큽니다"를 출력해주세요.
+
+		if(d1 > 3.01) {
+			System.out.println("d1은 3.01보다 큽니다");
+		}
 		
 		// 문제 31. d1의 값이 3.0 보다 크고 6.0보다 작은 범위안에 있는 경우, "d1은 3.0보다 크고 6.0보다 작습니다"를 출력하고
 		// 			그렇지 않는 경우 변수 d1의 값을 0으로 초기화해주세요.
+
+		if(d1 > 3.0 && d1 < 6.0) {
+			System.out.println("d1은 3.0보다 크고 6.0보다 작습니다");
+		}
 		
 		// 문제 32. d2의 값이 3.14보다 작은 경우 "d2의 값이 3.14보다 작습니다"를 출력해주세요.
+
+		if(d2 < 3.14) {
+			System.out.println("d2의 값이 3.14보다 작습니다");
+		}
 		
 		// 문제 33. d2의 값이 2.02보다 크거나 d2의 값이 4.02보다 작은 경우, "d2의 값이 2.02보다 크고 4.02보다 작습니다"를 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(d2 > 2.02 || d2 < 4.02) {
+			System.out.println("d2의 값이 2.02보다 크고 4.02보다 작습니다");
+		}
 		
 		// 문제 34. d2의 값이 0보다 큰 경우, 값 100으로 초기화해주세요.
+
+		if(d2 > 0) {
+			d2 = 100;
+		}
 		
 		// 문제 35. 변수 score의 값이 90점 이상인 경우 "본인의 이름 님, A입니다!"를 출력해주세요.
 		//			이때, 본인의 이름은 변수 name을 이용해서 출력해주세요.
+
+		if(score > 90) {
+			System.out.println(name + "님 A입니다!");
+		}
 		
 		// 문제 36. 변수 score의 값이 80점 이상 90점 미만인 경우 "본인의 이름 님, B입니다!"를 출력해주세요.
 		//			이때, 본인의 이름은 변수 name을 이용해서 출력해주세요.
+
+		if(score >= 80 && score < 90) {
+			System.out.println(name + "님 B입니다!");
+		}
 		
 		// 문제 37. 변수 score의 값이 70점 이상 80점 미만인 경우 "본인의 이름 님, C입니다!"를 출력해주세요.
 		//			이때, 본인의 이름은 변수 name을 이용해서 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(score >= 70 && score < 80) {
+			System.out.println(name + "님 C입니다!");
+		}
 		
 		// 문제 38. 변수 score의 값이 60점 이상 70점 미만인 경우 "본인의 이름 님, D입니다!"를 출력해주세요.
 		//			이때, 본인의 이름은 변수 name을 이용해서 출력해주세요.
+
+		if(score >= 60 && score < 70) {
+			System.out.println(name + "님 D입니다!");
+		}
 		
 		// 문제 39. 변수 score의 값이 60점 미만인 경우 "본인의 이름 님, F입니다!"를 출력해주세요.
 		//			이때, 본인의 이름은 변수 name을 이용해서 출력해주세요.
+
+		if(score < 60) {
+			System.out.println(name + "님 F입니다!");
+		}
 		
 		// 문제 40. 변수 score의 값이 90점 이상인 경우 변수 grade의 값을 "A"로 초기화하고,
 		//		변수 score의 값이 80점 이상 90점 미만 경우 변수 grade의 값을 "B"로 초기화하고,
@@ -185,6 +474,21 @@ public class Home {
 		//		변수 score의 값이 60점 미만 경우 변수 grade의 값을 "F"로 초기화하고,
 		// 		최종 "본인의 이름 님, 현재 성적은 ? 입니다"를 출력해주세요.
 		// 		이때, 본인의 이름은 변수 name을 이용해서 출력하고 현재 성적에 ?는 grade를 이용하여 출력해주세요.
+		System.out.println("--------------------------------------------------------");
+		
+		if(score >= 90) {
+			grade = "A";
+		} else if(score >= 80 && score < 90){
+			grade = "B"; 
+		} else if(score >= 70 && score < 80){
+			grade = "C";
+		} else if(score >= 60 && score < 70){
+			grade = "D";
+		} else {
+			grade = "F";
+		}
+		
+		System.out.println(name + "님, 현재 성적은 " + grade + " 입니다.");
 		
 		// 문제 41. 변수들을 이용하여 아래와 같이 출력해주세요.
 		// 변수 score, name, gender, age, introduction을 활용
@@ -193,5 +497,12 @@ public class Home {
 		// 나이 : 99
 		// 성적 : A
 		// 자기소개 : 잘부탁드립니다!
+		System.out.println("--------------------------------------------------------");
+		
+		System.out.println("이름 : " + name);
+		System.out.println("성별 : " + gender);
+		System.out.println("나이 : " + age);
+		System.out.println("성적 : " + score);
+		System.out.println("자기소개 : " + introduction);
 	}
 }
