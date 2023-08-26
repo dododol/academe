5af9443a20383c0c2cb9ce39b003e835e4a2a19c
DESKTOP-DOL60IT\PC-15 <k.do.dodong@gmail.com>
2023-08-26 17:51:19 +0900

    python day3

---
+++ 학원 수업/python/day02/Human.java
@@ -0,0 +1,11 @@
+package day02;
+
+public class Human extends Animal {
+	String job = "백수";
+	
+	void chippo(String job) {
+		this.job = job;
+	}
+	
+
+}
---
+++ 학원 수업/python/day02/OopTest2.java
@@ -0,0 +1,14 @@
+ package day02;
+
+public class OopTest2 {
+
+	public static void main(String[] args) {
+			
+		Animal ani = new Animal();
+		System.out.println("flagLife"+ani.flagLife);
+		ani.die();
+		System.out.println("flagLife"+ani.flagLife);
+		
+	} 
+	
+}
