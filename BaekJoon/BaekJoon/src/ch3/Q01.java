40a931a8323c03e1e9858828cbb7b9b245825420
STUDENT-PC\Student <k.do.dodong@gmail.com>
2024-03-15 09:21:11 +0900

    .

---
+++ BaekJoon/BaekJoon/src/ch3/Q01.java
@@ -0,0 +1,15 @@
+package ch3;
+
+import java.util.Scanner;
+
+/*
+N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+*/
+public class Q01 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(N=2; N>=9; )
+	}
+}
