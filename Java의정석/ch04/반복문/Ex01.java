f7dfd7f44cd2be4c9826ddf4520a363a785c3e9b
BOOK-B31RUEMMBD\USER <k.do.dodong@gmail.com>
2024-03-24 23:13:02 +0900

    .

---
+++ Java의정석/ch04/반복문/Ex01.java
@@ -0,0 +1,47 @@
+package ch04.반복문;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		int num = 0;
+
+		/*
+		 * 괄호{} 안의 내용을 5번 반복한다.
+		 */
+
+//		for(int i=1; i<=5; i++) {
+//			num = (int) (Math.random() * 6) + 1;
+//			System.out.println(num);
+//		}
+		
+		/*
+		 * 괄호{} 안의 내용을 10번 반복한다.
+		 */		
+//		for(int i = 1; i <=20; i++) {
+//			System.out.println(Math.random());
+//		}
+
+		/*
+		 * Quiz1. 1~10사이의 난수를 20개 출력하시오.
+		 * 1,2,3,4,5,6,7,8,9,10
+		 */
+//		for(int i = 1; i <=20; i++) {
+//			num = (int) (Math.random() * 10) + 1;
+//			System.out.println(num);
+//		}
+		
+//		for(int i = 1; i <=20; i++) {
+//			System.out.println((int)(Math.random()*10)+1); // 1<=x<11, 1~10
+//		}
+		
+		
+		/*
+		 * Quiz2. -5~5사이의 난수를 20개 출력하시오.
+		 * -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 (11개)
+		 */
+//		for(int i = 1; i <=20; i++) {
+//			System.out.println((int)(Math.random()*10)-5); // -5<=x<6, -5~5
+//		}
+		
+		
+	}
+}
