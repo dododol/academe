d71f0e7d8c2d81e09a131ab82f738cb26d8ccc0e
BOOK-B31RUEMMBD\USER <k.do.dodong@gmail.com>
2024-05-26 22:30:59 +0900

    김영한자바 While

---
+++ 개인 공부/김영한자바/Location/java-start/src/HelloJava2.java
@@ -0,0 +1,10 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {    //프로그램의 시작점 : main메소드
+
+        System.out.println("HelloJava1");
+        System.out.println("HelloJava2");
+        System.out.println("HelloJava3");
+        System.out.println("HelloJava4");
+    }
+}
--- 개인 공부/김영한자바/java-start/src/HelloJava3.java
+++
@@ -1,6 +0,0 @@
-public class HelloJava3 {
-
-    public static void main(String[] args) {    //프로그램의 시작점 : main메소드
-        System.out.println("HelloJava");
-    }
-}
