2e9cb7b698c313f45d51ac3040e32223a2d7cf11
BOOK-B31RUEMMBD\USER <k.do.dodong@gmail.com>
2024-08-23 20:35:51 +0900

    delete

--- 개인 공부/프로젝트세팅/intellij세팅/src/main/java/com/codingrecipe/member/repository/MemberRepository.java
+++ 개인 공부/프로젝트세팅/intellij세팅/src/main/java/com/codingrecipe/member/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository { //Repository class는 DB쪽과 연관이 있다. 우리는 Mybatis를 사용. 적절한 쿼리 호출 및 매개변수 넘겨주고, 리턴값이 있다면 리턴해주고 하는 역할.
@@ -16,5 +18,17 @@ public class MemberRepository { //Repository class는 DB쪽과 연관이 있다.
 
     public MemberDTO login(MemberDTO memberDTO) {
         return sql.selectOne("Member.login", memberDTO);
+    }
+
+    public List<MemberDTO> findAll() {
+        return sql.selectList("Member.findAll");
+    }
+
+    public MemberDTO findById(Long id) {
+        return sql.selectOne("Member.findById", id);
+    }
+
+    public void delete(Long id) {
+        sql.delete("Member.delete", id);
     }
 }
